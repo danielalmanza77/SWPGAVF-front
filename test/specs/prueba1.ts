ab29d9b37ef9172a7528141ae514a8740a9211b0
cotox3d <165819989+cotox3d@users.noreply.github.com>
2024-10-04 08:42:00 +0000

    webdriverio2

--- test/specs/prueba1.ts
+++
@@ -1,7 +0,0 @@
-describe('Mi primera prueba en WebdriverIO', () => {
-    it('debería tener el título correcto', async () => {
-        await browser.url('http://localhost:5173');
-        const title = await browser.getTitle();
-        expect(title).toBe('Título de tu aplicación');
-    });
-});
