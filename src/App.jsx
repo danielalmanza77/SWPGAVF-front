ed8d91b6be5eb8ca85eb8ec0f52e860e8da109b3
danielalmanza77 <daniel.almanza@urp.edu.pe>
2024-10-05 23:13:03 -0500

    gestionar-productos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingLayout from './layouts/LandingLayout'
 import Products from './pages/landing/products/Products'
 import Home from './pages/landing/home/Home'
 import GestionarKardex from './pages/warehouse/GestionarKardex'
+import GestionarProductos from './pages/warehouse/gestionarProductos/GestionarProductos'
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path='products' element={<Products />} />
           </Route>
-          <Route path='/warehouse' element={<GestionarKardex />} />
+          {/* <Route path='/warehouse' element={<GestionarKardex />} /> */}
+          <Route path='/warehouse' element={<GestionarProductos />} />
         </Routes>
       </BrowserRouter>
     </>
